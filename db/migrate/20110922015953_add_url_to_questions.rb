c4a68a3c7702c0789e6a0fe40fd300ac47018ac0
lehung <truonglehung@gmail.com>
2011-09-27 20:41:14 +0700

    ba commit

---
+++ db/migrate/20110922015953_add_url_to_questions.rb
@@ -0,0 +1,10 @@
+class AddUrlToQuestions < ActiveRecord::Migration
+  def self.up
+    add_column :questions, :url_question, :string
+  end
+
+  def self.down
+    remove_column :questions, :url_answers
+    remove_column :questions, :url_question
+  end
+end
